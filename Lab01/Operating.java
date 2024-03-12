3e3e089170bf68e6167b1739b4149b6db7a86767
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-03-12 11:00:02 +0700

    Complete Assignment 2.2.5

--- Lab01/Operating.java
+++ Lab01/Operating.java
@@ -1,23 +1,26 @@
 import javax.swing.JOptionPane;
-public class ShowTwoNumbers{
+public class Operating{
     public static void main(String[] args){
         String strNum1, strNum2;
-        String strNotification = "You've just entered: ";
 
         strNum1 = JOptionPane.showInputDialog(null,
         "Please input the first number:", "Input the first number",
         JOptionPane.INFORMATION_MESSAGE);
 
-        strNotification += strNum1 + "and ";
-
         strNum2 = JOptionPane.showInputDialog(null,
         "Please input the first number:", "Input the second number",
         JOptionPane.INFORMATION_MESSAGE);
+        
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
 
-        strNotification += strNum2;
+        double sumation = num1 + num2;
+        double diff = num1 - num2;
 
-        JOptionPane.showMessageDialog(null, strNotification,
-        "Show two numbers ", JOptionPane.INFORMATION_MESSAGE);
+        double prod = num1 * num2;
+        double quotient = num1 / num2;
+
+        JOptionPane.showMessageDialog(null,"Sum : " + sumation + "\nDiff : " + diff + "\nProd : " + prod + "\nQuot : " + quotient,"Operating results", JOptionPane.INFORMATION_MESSAGE);
 
         System.exit(0);
     }
