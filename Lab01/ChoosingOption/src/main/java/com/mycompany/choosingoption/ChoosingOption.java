d28f1642f1b456eec1de0bbe8422fb2b825f5350
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-03-13 15:48:36 +0700

    Complete Assignment 6.1

---
+++ Lab01/ChoosingOption/src/main/java/com/mycompany/choosingoption/ChoosingOption.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.choosingoption;
+
+/**
+ *
+ * @author khangpt
+ */
+import javax.swing.JOptionPane;
+public class ChoosingOption {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null,
+                "Do you want to change to the first class ticket?");
+        JOptionPane.showMessageDialog(null, "You've chosen: "+ (option==JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    }
+}
