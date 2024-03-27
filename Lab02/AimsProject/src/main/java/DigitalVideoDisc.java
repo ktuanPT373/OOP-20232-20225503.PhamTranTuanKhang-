3b06619e2512cf1f0682994ac69c257df7ca3c3e
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-03-27 17:22:17 +0700

    update section 7,8,9,10

--- Lab02/AimsProject/src/main/java/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/main/java/DigitalVideoDisc.java
@@ -4,7 +4,7 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
-    
+
     /**
      * @return the title
      */
@@ -39,5 +39,29 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
-
+    public DigitalVideoDisc(String title){
+        super();
+        this.title = title;
+    }
+    public DigitalVideoDisc(String category, String title, float cost){
+        super();
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String director, String category, String title, float cost){
+        super();
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
 }
