31448a27e432597f7c21ab776e4d4e3f6974b72f
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-03-27 16:11:38 +0700

    update 12

---
+++ Lab02/AimsProject/src/main/java/Aims.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author khangpt
+ */
+public class Aims {
+    public static void main(String[] args){
+        
+        // Create a new class
+        // Create a new class
+        Cart anOrder = new Cart();
+
+        // Create new dvd object and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);  
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Start Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);  
+        
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        
+        System.out.println("Total cost is: ");
+        System.out.println(anOrder.totalCost());
+        
+    }
+}
---
+++ Lab02/AimsProject/src/main/java/Cart.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author khangpt
+ */
+public class Cart {
+    
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered;
+    
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if (qtyOrdered < MAX_NUMBERS_ORDERED){
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("This disc " + disc.getTitle() + "has been added to cart");
+            if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+                System.out.println("Warning : The cart is almost full");
+            }
+        }else {
+            System.out.println("Error : The cart is full, cound not add the disc "+disc.getTitle() + " to the cart");
+        }
+    }
+    
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        boolean found = false;
+        
+        for (int i = 0; i < qtyOrdered; i++){
+            if (itemsOrdered[i].equals(disc)){
+                found = true;
+                for (int j = i; j < qtyOrdered; j++){
+                    itemsOrdered[j] = itemsOrdered[j+1];
+                }
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+                System.out.println("The disc "+ disc.getTitle() + " has been removed from the cart");
+            }
+        }
+        if (!found){
+            System.out.println("The disc " + disc.getTitle() + "is not found in the cart");
+        }
+    }
+    
+    public double totalCost(){
+        double total = 0.0;
+        for (int i = 0; i < qtyOrdered; i++){
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
