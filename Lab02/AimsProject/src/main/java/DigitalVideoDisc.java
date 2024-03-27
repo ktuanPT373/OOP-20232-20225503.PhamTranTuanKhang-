7b516b3f7fbec846d0b564314733c04bc3115c39
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-03-27 17:16:51 +0700

    update section 9

--- Lab02/AimsProject/src/main/java/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/main/java/DigitalVideoDisc.java
@@ -4,4 +4,40 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+    
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @return the director
+     */
+    public String getDirector() {
+        return director;
+    }
+
+    /**
+     * @return the length
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * @return the cost
+     */
+    public float getCost() {
+        return cost;
+    }
+
 }
