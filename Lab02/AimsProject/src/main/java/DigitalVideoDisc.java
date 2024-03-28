4f5e53b645ca6219b00f7174e2e011d44601de84
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-03-28 15:00:53 +0700

    update passing parameters

--- Lab02/AimsProject/src/main/java/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/main/java/DigitalVideoDisc.java
@@ -14,41 +14,54 @@ public class DigitalVideoDisc {
     private int length;
     private float cost;
 
-    /**
-     * @return the title
-     */
     public String getTitle() {
         return title;
     }
 
-    /**
-     * @return the category
-     */
-    public String getCategory() {
-        return category;
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDirectory() {
+		return director;
+	}
+	public void setDirectory(String director) {
+		this.director = director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+    public DigitalVideoDisc(String title){
+        super();
+        this.title = title;
     }
-
-    /**
-     * @return the director
-     */
-    public String getDirector() {
-        return director;
+    public DigitalVideoDisc(String category, String title, float cost){
+        super();
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
     }
-
-    /**
-     * @return the length
-     */
-    public int getLength() {
-        return length;
+    public DigitalVideoDisc(String director, String category, String title, float cost){
+        super();
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
     }
-
-    /**
-     * @return the cost
-     */
-    public float getCost() {
-        return cost;
-    }
-
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         super();
         this.title = title;
@@ -57,8 +70,4 @@ public class DigitalVideoDisc {
         this.length = length;
         this.cost = cost;
     }
-    public DigitalVideoDisc(String title, String category, float cost) {
-        this(title, category, "Unknown", 0, cost);  // Call the full constructor with defaults
-    }
-    
 }
