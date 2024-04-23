6281c6008c14faab485f53f0b71b56cf0e8b4769
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-04-23 11:43:17 +0700

    update

--- Lab02/AimsProject/src/main/java/hust/soict/dsai/aims/Aims.java
+++ Lab02/AimsProject/src/main/java/hust/soict/dsai/aims/Aims.java
@@ -2,6 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package hust.soict.dsai.aims;
+
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
 /**
  *
--- Lab02/AimsProject/src/main/java/hust/soict/dsai/aims/cart/Cart.java
+++ Lab02/AimsProject/src/main/java/hust/soict/dsai/aims/cart/Cart.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package hust.soict.dsai.aims.cart;
+
+import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
 /**
  *
